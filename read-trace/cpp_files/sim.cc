f3fa9ac5c36c17dbe1df75b920e2a688e939ebf8
sudarshansdr <sudarshanrajkumar5191@gmail.com>
2024-09-11 16:41:49 -0400

    Added Read-Trace files
    Added Read-Trace files from project template at Moodle for C++

---
+++ read-trace/cpp_files/sim.cc
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <inttypes.h>
+#include "sim.h"
+
+/*  "argc" holds the number of command-line arguments.
+    "argv[]" holds the arguments themselves.
+
+    Example:
+    ./sim 32 8192 4 262144 8 3 10 gcc_trace.txt
+    argc = 9
+    argv[0] = "./sim"
+    argv[1] = "32"
+    argv[2] = "8192"
+    ... and so on
+*/
+int main (int argc, char *argv[]) {
+   FILE *fp;			// File pointer.
+   char *trace_file;		// This variable holds the trace file name.
+   cache_params_t params;	// Look at the sim.h header file for the definition of struct cache_params_t.
+   char rw;			// This variable holds the request's type (read or write) obtained from the trace.
+   uint32_t addr;		// This variable holds the request's address obtained from the trace.
+				// The header file <inttypes.h> above defines signed and unsigned integers of various sizes in a machine-agnostic way.  "uint32_t" is an unsigned integer of 32 bits.
+
+   // Exit with an error if the number of command-line arguments is incorrect.
+   if (argc != 9) {
+      printf("Error: Expected 8 command-line arguments but was provided %d.\n", (argc - 1));
+      exit(EXIT_FAILURE);
+   }
+    
+   // "atoi()" (included by <stdlib.h>) converts a string (char *) to an integer (int).
+   params.BLOCKSIZE = (uint32_t) atoi(argv[1]);
+   params.L1_SIZE   = (uint32_t) atoi(argv[2]);
+   params.L1_ASSOC  = (uint32_t) atoi(argv[3]);
+   params.L2_SIZE   = (uint32_t) atoi(argv[4]);
+   params.L2_ASSOC  = (uint32_t) atoi(argv[5]);
+   params.PREF_N    = (uint32_t) atoi(argv[6]);
+   params.PREF_M    = (uint32_t) atoi(argv[7]);
+   trace_file       = argv[8];
+
+   // Open the trace file for reading.
+   fp = fopen(trace_file, "r");
+   if (fp == (FILE *) NULL) {
+      // Exit with an error if file open failed.
+      printf("Error: Unable to open file %s\n", trace_file);
+      exit(EXIT_FAILURE);
+   }
+    
+   // Print simulator configuration.
+   printf("===== Simulator configuration =====\n");
+   printf("BLOCKSIZE:  %u\n", params.BLOCKSIZE);
+   printf("L1_SIZE:    %u\n", params.L1_SIZE);
+   printf("L1_ASSOC:   %u\n", params.L1_ASSOC);
+   printf("L2_SIZE:    %u\n", params.L2_SIZE);
+   printf("L2_ASSOC:   %u\n", params.L2_ASSOC);
+   printf("PREF_N:     %u\n", params.PREF_N);
+   printf("PREF_M:     %u\n", params.PREF_M);
+   printf("trace_file: %s\n", trace_file);
+   printf("\n");
+
+   // Read requests from the trace file and echo them back.
+   while (fscanf(fp, "%c %x\n", &rw, &addr) == 2) {	// Stay in the loop if fscanf() successfully parsed two tokens as specified.
+      if (rw == 'r')
+         printf("r %x\n", addr);
+      else if (rw == 'w')
+         printf("w %x\n", addr);
+      else {
+         printf("Error: Unknown request type %c.\n", rw);
+	 exit(EXIT_FAILURE);
+      }
+
+      ///////////////////////////////////////////////////////
+      // Issue the request to the L1 cache instance here.
+      ///////////////////////////////////////////////////////
+    }
+
+    return(0);
+}
